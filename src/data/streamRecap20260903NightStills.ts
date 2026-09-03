abfe038635bd38640089bb7def20fe740fa7e26b
ackey1007fw-coder <ackey1007fw@gmail.com>
2026-09-03 16:16:42 -0700

    Keep night still aggregator on existing stills 01-02 until 03-10 land.

--- src/data/streamRecap20260903NightStills.ts
+++ src/data/streamRecap20260903NightStills.ts
@@ -1,24 +1,8 @@
 import type { StreamRecapImage } from "./streamRecaps";
 import { gachiNightStill01 } from "./streamRecap20260903NightStill01.ts";
 import { gachiNightStill02 } from "./streamRecap20260903NightStill02.ts";
-import { gachiNightStill03 } from "./streamRecap20260903NightStill03.ts";
-import { gachiNightStill04 } from "./streamRecap20260903NightStill04.ts";
-import { gachiNightStill05 } from "./streamRecap20260903NightStill05.ts";
-import { gachiNightStill06 } from "./streamRecap20260903NightStill06.ts";
-import { gachiNightStill07 } from "./streamRecap20260903NightStill07.ts";
-import { gachiNightStill08 } from "./streamRecap20260903NightStill08.ts";
-import { gachiNightStill09 } from "./streamRecap20260903NightStill09.ts";
-import { gachiNightStill10 } from "./streamRecap20260903NightStill10.ts";
 
 export const gachiNightStills: StreamRecapImage[] = [
   gachiNightStill01,
   gachiNightStill02,
-  gachiNightStill03,
-  gachiNightStill04,
-  gachiNightStill05,
-  gachiNightStill06,
-  gachiNightStill07,
-  gachiNightStill08,
-  gachiNightStill09,
-  gachiNightStill10,
 ];
